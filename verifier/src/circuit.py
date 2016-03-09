7ed78aaa590fab9c2a683375b890796cd98ccbcb
so4pmaker <knsn1994@gmail.com>
2016-03-09 19:33:18 +0530

    constraints are now processed as infix (converted to postfix internally)

--- verifier/src/circuit.py
+++ verifier/src/circuit.py
@@ -1,14 +1,67 @@
+# mapping contains description of every supported variable
+# FORMAT FOR DESCRIPTIONS:
+# (arity, Eval: lis -> value)
+# where Eval is the evaluation function
+# TODO: make a separate class for operators
+mapping = {
+  '+': (2, lambda x: x[0] + x[1], 5),
+  '-': (2, lambda x: x[0] - x[1], 5),
+  '*': (2, lambda x: x[0] * x[1], 6),
+  '/': (2, lambda x: x[0] / x[1], 6),
+  '||': (2, lambda x: x[0] or x[1], 2),
+  '&&': (2, lambda x: x[0] and x[1], 3),
+  '>': (2, lambda x: x[0] > x[1], 4),
+  '<': (2, lambda x: x[0] < x[1], 4),
+  '>=': (2, lambda x: x[0] >= x[1], 4),
+  '<=': (2, lambda x: x[0] <= x[1], 4),
+  '!': (2, lambda x: not x[0], 5)
+}
+
 # Circuit class emulates circuit of one property
 # in FPGA. The assumptions here are that the description
 # of input property is in postfix notation. We can easily support
 # infix notation by writing a convertor later.;
 
 class Circuit:
+  def infix_to_postfix(self, infixexpr):
+    def getPref(operator):
+      if operator == '(':
+        return 1
+      return mapping[operator][2]
+
+    opStack = []
+    postfixList = []
+    tokenList = infixexpr.split()
+
+    for token in tokenList:
+      if token == '(':
+        opStack.append(token)
+
+      elif token == ')':
+        topToken = opStack.pop()
+        while topToken != '(':
+          postfixList.append(topToken)
+          topToken = opStack.pop()
+
+      elif token in mapping:
+        while len(opStack) <> 0 and (getPerf(opStack[-1]) >= getPerf(token)):
+          postfixList.append(opStack.pop())
+        opStack.append(token)
+      else:
+        postfixList.append(token)
+
+    while len(opStack) <> 0:
+        postfixList.append(opStack.pop())
+    return postfixList
+
+
   def __init__(self, constraint):
     # state stores the values of various variables
     self.state = {}
+
     # constraint saves the property
-    self.constraint = constraint.strip().split()
+    self.infix = constraint.strip()
+    self.constraint = self.infix_to_postfix(self.infix)
 
   # update the value of a certain variable
   def update(self, key, value):
@@ -30,25 +83,6 @@ class Circuit:
           return None
 
   def verify(self):
-    # mapping contains description of every supported variable
-    # FORMAT FOR DESCRIPTIONS:
-    # (arity, Eval: lis -> value)
-    # where Eval is the evaluation function
-    # TODO: make a separate class for operators
-    mapping = {
-      '+': (2, lambda x: x[0] + x[1]),
-      '-': (2, lambda x: x[0] - x[1]),
-      '*': (2, lambda x: x[0] * x[1]),
-      '/': (2, lambda x: x[0] / x[1]),
-      '||': (2, lambda x: x[0] or x[1]),
-      '&&': (2, lambda x: x[0] and x[1]),
-      '>': (2, lambda x: x[0] > x[1]),
-      '<': (2, lambda x: x[0] < x[1]),
-      '>=': (2, lambda x: x[0] >= x[1]),
-      '<=': (2, lambda x: x[0] <= x[1]),
-      '!': (2, lambda x: not x[0])
-    }
-
     # Three simple functions to consume the mapping dictionary
     def isOperator(symbol):
       return symbol in mapping
@@ -88,7 +122,9 @@ class Circuit:
 
 
 if __name__ == '__main__':
-  c = Circuit('1 x <')
+  c = Circuit('1 < x')
+  print c.infix
+  print c.constraint
   c.update('x', 2)
   print c.verify()
   c.update('x', 0)
