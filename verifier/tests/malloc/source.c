bbd6a8cb4d7e5923de3f89749f8fbba245b6b602
so4pmaker <knsn1994@gmail.com>
2016-03-09 19:36:08 +0530

    tests changed to infix

--- verifier/tests/malloc/source.c
+++ verifier/tests/malloc/source.c
@@ -3,13 +3,13 @@
 unsigned int page_alloc(int num_of_ints){
   int* ret = (int*) malloc( num_of_ints << 2 );
   if ( ret == NULL ) return 0;
-  return (int) ret; 
+  return (int) ret;
 }
 
 int main(){
   unsigned int page_address = page_alloc(100);
 
-  //** page_address 0 !=
+  //** page_address != 0
 
-  return 0; 
+  return 0;
 }
